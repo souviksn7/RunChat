b1af323b9c21fd19792f303a1ca0d115de2b7e79
Souvik <souviknandi235@gmail.com>
2022-08-22 09:05:50 +0530

    Added routes and functions

--- Backend/routes/chatRoutes.js
+++ Backend/routes/chatRoutes.js
@@ -1,21 +1,21 @@
 const express = require("express");
-const { protect } = require("../middleware/authMiddleware");
 const {
   accessChat,
   fetchChats,
   createGroupChat,
-  renameGroup,
+  removeFromGroup,
   addToGroup,
-  removeFromgroup,
+  renameGroup,
 } = require("../controllers/chatControllers");
+const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.route("/").post(protect, accessChat);
 router.route("/").get(protect, fetchChats);
 router.route("/group").post(protect, createGroupChat);
-router.route("/remane").put(protect, renameGroup);
+router.route("/rename").put(protect, renameGroup);
+router.route("/groupremove").put(protect, removeFromGroup);
 router.route("/groupadd").put(protect, addToGroup);
-router.route("/groupremove").put(protect, removeFromgroup);
 
 module.exports = router;
