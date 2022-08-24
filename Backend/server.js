fae9a773dd256752468c605b440afa1d418d37b6
Souvik <souviknandi235@gmail.com>
2022-08-24 13:33:58 +0530

    Created Group chat UI, Seding message api

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,7 @@ const { chats } = require("./data/data.js");
 const connectDB = require("./config/db.js");
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
+const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 dotenv.config();
@@ -17,6 +18,7 @@ app.get("/", (req, res) => {
 });
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
+app.use("/api/Message", messageRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
